4523ce67b6bfa062d4e4c6dd6abcab9089b95cbf
JuventusF <785751385@qq.com>
2023-05-13 08:05:07 +0800

    原版网页地图，缺css

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="ZH-cn">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <title>World Map</title>
+</head>
+<body>
+<header>
+    <h1>World Map</h1>
+</header>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">World Map</a>
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" data-toggle="dropdown"
+                       aria-haspopup="true" aria-expanded="false">
+                        Asia
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">China</a>
+                        <a class="dropdown-item" href="#">India</a>
+                        <a class="dropdown-item" href="#">Japan</a>
+                    </div>
+                </li>
+            </ul>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" data-toggle="dropdown"
+                   aria-haspopup="true" aria-expanded="false">
+                    Europe
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">France</a>
+                    <a class="dropdown-item" href="#">Germany</a>
+                    <a class="dropdown-item" href="#">Italy</a>
+                </div>
+            </li>
+            </ul>
+        </div>
+    </div>
+    <main>
+        <div class="container-fluid">
+            <h2>Asia</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                incididunt ut labore et dolore magna aliqua.
+            </p>
+        </div>
+        <div class="container-fluid ">
+            <h2>Europe</h2>
+            <p>
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                commodo consequat.
+            </p>
+            <p>
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur.
+            </p>
+        </div>
+    </main>
+</body>
+</html>
+
